a7498ea88731b8017898d431fa93769c94f881a8
PlatonKynin <kininplaton@gmail.com>
2024-05-21 23:13:01 +0300

    02Sprint - строчки 59 -> вариант решения -> есть убрать filter: 'all' v2 + рефакторинг в несколько строк

--- src/App.tsx
+++ src/App.tsx
@@ -4,94 +4,98 @@ import {useState} from "react";
 import {v1} from "uuid";
 
 export type TaskType = {
-	id: string
-	title: string
-	isDone: boolean
+    id: string
+    title: string
+    isDone: boolean
 }
 
 export type TodolistType = {
-	id: string,
-	title: string
-	filter: FilterValuesType
+    id: string,
+    title: string
+    filter: FilterValuesType
 }
 
 export type FilterValuesType = 'all' | 'active' | 'completed'
 
 function App() {
 
-	let [todolists, setTodolists] = useState<TodolistType[]>([
-		{ id: v1(), title: 'What to learn', filter: 'all' },
-		{ id: v1(), title: 'What to buy', filter: 'all' },
-	])
+    let [todolists, setTodolists] = useState<TodolistType[]>([
+        {id: v1(), title: 'What to learn', filter: 'all'},
+        {id: v1(), title: 'What to buy', filter: 'all'},
+    ])
 
-	const [tasks, setTasks] = useState<TaskType[]>([
-		{id: v1(), title: 'HTML&CSS', isDone: true},
-		{id: v1(), title: 'JS', isDone: true},
-		{id: v1(), title: 'ReactJS', isDone: false},
-	])
+    const [tasks, setTasks] = useState<TaskType[]>([
+        {id: v1(), title: 'HTML&CSS', isDone: true},
+        {id: v1(), title: 'JS', isDone: true},
+        {id: v1(), title: 'ReactJS', isDone: false},
+    ])
 
-	// const [filter, setFilter] = useState<FilterValuesType>('all')
+    // const [filter, setFilter] = useState<FilterValuesType>('all')
 
-	const removeTask = (taskId: string) => {
-		const filteredTasks = tasks.filter((task) => {
-			return task.id !== taskId
-		})
-		setTasks(filteredTasks)
-	}
+    const removeTask = (taskId: string) => {
+        const filteredTasks = tasks.filter((task) => {
+            return task.id !== taskId
+        })
+        setTasks(filteredTasks)
+    }
 
-	const addTask = (title: string) => {
-		const newTask = {
-			id: v1(),
-			title: title,
-			isDone: false
-		}
-		const newTasks = [newTask, ...tasks]
-		setTasks(newTasks)
-	}
+    const addTask = (title: string) => {
+        const newTask = {
+            id: v1(),
+            title: title,
+            isDone: false
+        }
+        const newTasks = [newTask, ...tasks]
+        setTasks(newTasks)
+    }
 
-	const changeTaskStatus = (taskId: string, taskStatus: boolean) => {
-		const newState = tasks.map(t => t.id == taskId ? {...t, isDone: taskStatus} : t)
-		setTasks(newState)
-	}
-	//	{id, title, filter, filter:'completed'} === {...el}
-	// [[]] убираем лишний массив, так как map и так создаёт новый массив
-	const changeFilter = (todolistId: string, filterValue: FilterValuesType) => {
-		setTodolists(todolists.map(el=> el.id===todolistId ? {id: v1(), title: 'What to learn', filter: 'all', filter:filterValue} : el))
+    const changeTaskStatus = (taskId: string, taskStatus: boolean) => {
+        const newState = tasks.map(t => t.id == taskId ? {...t, isDone: taskStatus} : t)
+        setTasks(newState)
+    }
+    //	{id, title, filter, filter:'completed'} === {...el}
+    // [[]] убираем лишний массив, так как map и так создаёт новый массив
+    const changeFilter = (todolistId: string, filterValue: FilterValuesType) => {
+        setTodolists(todolists.map(
+            el => el.id === todolistId
+                ? {id: v1(), title: 'What to learn', filter: filterValue}
+                : el
+        ))
 
-		// const currentTodolist = todolists.find(el => el.id === todolistId)
-		// console.log(currentTodolist)
-		// if (currentTodolist) {
-		// 	currentTodolist.filter = filterValue
-		// 	setTodolists([...todolists]) // Передаём новый массив-матрёшек, чтобы реакт проснулся
-		// 	// console.log(todolists)
-		// }
-	}
+        // const currentTodolist = todolists.find(el => el.id === todolistId)
+        // console.log(currentTodolist)
+        // if (currentTodolist) {
+        // 	currentTodolist.filter = filterValue
+        // 	setTodolists([...todolists]) // Передаём новый массив-матрёшек, чтобы реакт проснулся
+        // 	// console.log(todolists)
+        // }
+    }
 
-	return (
-		<div className="App">
-			{todolists.map((el) => {
-				let tasksForTodolist = tasks
-				if (el.filter === 'active') {
-					tasksForTodolist = tasks.filter(task => !task.isDone)
-				}
-				if (el.filter === 'completed') {
-					tasksForTodolist = tasks.filter(task => task.isDone)
-				}
-				return <Todolist
-					key={el.id}
-					todolistId={el.id}
-					title={el.title}
-					tasks={tasksForTodolist}
-					removeTask={removeTask}
-					changeFilter={changeFilter}
-					addTask={addTask}
-					changeTaskStatus={changeTaskStatus}
-					filter={el.filter}
-				/>
-			})}
+    return (
+        <div className="App">
+            {todolists.map((el) => {
+                let tasksForTodolist = tasks
+                if (el.filter === 'active') {
+                    tasksForTodolist = tasks.filter(task => !task.isDone)
+                }
+                if (el.filter === 'completed') {
+                    tasksForTodolist = tasks.filter(task => task.isDone)
+                }
+                return <Todolist
+                    key={el.id}
+                    todolistId={el.id}
+                    title={el.title}
+                    tasks={tasksForTodolist}
+                    removeTask={removeTask}
+                    changeFilter={changeFilter}
+                    addTask={addTask}
+                    changeTaskStatus={changeTaskStatus}
+                    filter={el.filter}
+                />
+            })}
 
-		</div>
-	);
+        </div>
+    );
 }
 
 export default App;
