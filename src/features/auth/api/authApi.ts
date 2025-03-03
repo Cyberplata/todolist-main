b933b8806d85187bab6ca06e86728bf3fd50212c
PlatonKynin <kininplaton@gmail.com>
2025-03-03 21:40:04 +0300

    04Sprint-4week - вариант с передачей headers в config

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -5,6 +5,12 @@ import type { LoginArgs } from "./authApi.types"
 export const authApi = {
    login(payload: LoginArgs) {
       // debugger
-      return instance.post<BaseResponse<{ userId: number; token: string }>>(`auth/login`, payload)
+      // return instance.post<BaseResponse<{ userId: number; token: string }>>(`auth/login`, payload)
+      return instance.post<BaseResponse<{ userId: number; token: string }>>(`auth/login`, payload, {
+         // вариант с передачей headers в config
+         headers: {
+            Authorization: `Bearer ${localStorage.getItem("sn-token") || ""}`,
+         },
+      })
    },
 }
